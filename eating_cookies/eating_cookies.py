845e6009fe27d3fbb861fb7523f1e3728bd8a287
Anthony Hill <hillan1152@gmail.com>
2020-03-19 07:01:24 -0400

    Understand and plan for recipe batches

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,54 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+# UNDERSTAND
+#   CAN EAT 0, 1, 2, OR 3
+#   NEED TO COUNT NUMBER OF **POSSIBLE** WAYS TO COUNT
+#   CACHE USED TO KEEP TRACK OF REPITIION
+#   MATH TYPES:
+#     IT CAN BE DIVISIBLE BY 3
+#     CAN HAVE THE REMAINDER BE DIVIDED OTHER NUMBERS
+
+
 def eating_cookies(n, cache=None):
-  pass
+    # POSSIBLE WAYS = 0
+    # TAKE N
+    total_cookies = n
+    ways = 0
+    # IF 0 RETURN ZERO
+    if n <= 0:
+        pass
+    # IF N IS DIVISIBLE BY 1
+    if n % 1 == 0:
+        # ADD 1 WAYS
+        ways += 1
+    # IF N IS DIVISIBLE BY 2
+    if n % 2 == 0:
+        # ADD 1 WAYS
+        ways += 1
+    if n % 3 == 0:
+        ways += 1
+    #   IF REMAINDER/3
+    #     ADD 1 WAYS
+    #   IF N IS DIVISIBLE BY 1
+    #   ADD 1 WAYS
+    # IF N IS DIVISIBLE BY 3
+    #   ADD 1 WAYS
+    #   IF REMAINDER/3
+    #     ADD 1 WAYS
+    # Return ways
+
+    return ways
+
+
+print(eating_cookies(6))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python
 
 import math
+# UNDERSTAND
+#   Take in a DICTIONARY of a RECIPE and AVAILABLE INGREDIENTS
+#     Includes key - values: name - amount
+#   RECIPE DICTIONARY:
+#     amount = amt of each ingredient needed for the recipe
+#   INGREDIENTS DICTIONARY:
+#     amount = amt of the ingredient available to you
+#   GOAL/NEED: Output max number of WHOLE BATCHES
+#      2nd Dictionary represents ingredients dictionary.
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    # PLAN
+    #   keep track of number of batches available
+    # recipe_amt =
+    #   keep track of ingredient amt
+    #   keep track of recipe amt
+    #
+    #   If values of ingredients (in the entire dictionary) are divisible by #    values of recipe continue
+    #
+    pass
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
